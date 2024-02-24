1e78401e4245bd1f743aa9f4c7cd7e405eec3704
Lotte-Et <charlotte.stowe@gmail.com>
2024-02-24 09:35:30 -0500

    Started to implement inquirer; Updated package.json with dependencies

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 //require ./lib/shapes.js
-
+const { shapes } = require('./lib/shapes');
 //require inquirer
-
+const inquirer = require('inquirer');
 //require fs
-
+const { writeFile } = require('fs/promises');
 //Inquirer prompts
+inquirer.prompt([
+    {
+        name: 'text',
+        message: 'What should the text be on your image? (max. 3 characters)',
+    },
+    {
+        name: 'textColor',
+        message: 'What color should your text be?'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape should your logo be?',
+        choices: ['Circle', 'Square', 'Star']
+    },
+    {
+        name: 'shapeColor',
+        message: 'What color should your logo shape be?'
+    }
+]).then(console.log('Complete'))
 //Text max three characters
 //Text color
 //Shape (pick list)
