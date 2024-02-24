655b450894c8aaf775a70ecec8c8a2f529190c79
Lotte-Et <charlotte.stowe@gmail.com>
2024-02-23 19:08:41 -0500

    Adding base files; Added comments to some files; Example svg file in /lib/more/

---
+++ index.js
@@ -0,0 +1,15 @@
+//require ./lib/shapes.js
+
+//require inquirer
+
+//require fs
+
+//Inquirer prompts
+//Text max three characters
+//Text color
+//Shape (pick list)
+//Shape color
+
+//Generate .svg files as logo.svg
+
+//Console log 'Generated logo.svg'
